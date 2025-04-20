2d1d97cf5ff67cbff0104954d02d4e782ba69731
Douglas Belarmino <douglas.belarr@gmail.com>
2025-04-20 12:56:55 -0300

    add clear button

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = "treino-tracker-v1";
+const urlsToCache = [
+  "/treino-tracker/",
+  "/treino-tracker/index.html",
+  "/treino-tracker/icon.png",
+  "/treino-tracker/icon-512.png",
+  "https://cdn.tailwindcss.com",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName);
+          }
+        })
+      )
+    )
+  );
+});
